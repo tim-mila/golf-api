485a575ca11ae610df94aefb666024b4d782cc71
tim <timothy.mila@gmail.com>
2026-02-05 10:00:37 -0600

    fix(*): Extend range for course rating validation

--- src/main/java/com/alimmit/golf/scorecard/CourseRatingValidatorImpl.java
+++ src/main/java/com/alimmit/golf/scorecard/CourseRatingValidatorImpl.java
@@ -4,7 +4,7 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 /**
- * Course rating is relative to the course par, typically par +/- 5
+ * Course rating is relative to the course par, typically par +/- 10
  */
 class CourseRatingValidatorImpl implements ConstraintValidator<ValidRating, ScorecardRequestDto> {
 
@@ -12,6 +12,6 @@ class CourseRatingValidatorImpl implements ConstraintValidator<ValidRating, Scor
   public boolean isValid(ScorecardRequestDto scorecardRequestDto, ConstraintValidatorContext constraintValidatorContext) {
     Double rating = scorecardRequestDto.rating();
     Integer par = scorecardRequestDto.par();
-    return (rating != null && par != null) && (rating <= par + 5) && (rating >= par - 5);
+    return (rating != null && par != null) && (rating <= par + 10) && (rating >= par - 10);
   }
 }
