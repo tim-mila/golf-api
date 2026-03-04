1152a2227f4b4aefa2d4801899499887243794bd
tim <timothy.mila@gmail.com>
2026-03-03 21:39:13 -0600

    fix: Remove optional argument, it's a code smell

--- src/main/java/com/alimmit/golf/differential/DifferentialCalculatorImpl.java
+++ src/main/java/com/alimmit/golf/differential/DifferentialCalculatorImpl.java
@@ -36,7 +36,7 @@ class DifferentialCalculatorImpl implements DifferentialCalculator {
     // differential using the expected score.
     // https://www.usga.org/content/usga/home-page/handicapping/world-handicap-system/2024-revision/2024-treatment-of-9-hole-scores-FAQ.html
     else if (ScorecardType.NINE == scorecard.scorecardType() && handicap.isPresent()) {
-      double expectedDifferential = expectedDifferential(handicap);
+      double expectedDifferential = expectedDifferential(handicap.map(HandicapDto::handicapIndex).orElse(0.0));
       differential = new Differential(standardDifferential + expectedDifferential, true);
     }
     // For nine hole scores without an established handicap index use the standard differential
@@ -66,8 +66,7 @@ class DifferentialCalculatorImpl implements DifferentialCalculator {
    * @see <a
    *     href="https://www.usga.org/content/dam/usga/images/handicapping/infographics/Understanding%20Expected%20Score%20Infographic.pdf">https://www.usga.org/content/dam/usga/images/handicapping/infographics/Understanding%20Expected%20Score%20Infographic.pdf</a>
    */
-  private double expectedDifferential(Optional<HandicapDto> handicap) {
-    double index = handicap.map(HandicapDto::handicapIndex).orElse(0.0);
+  private double expectedDifferential(Double index) {
     double differential = index * 0.52 + 1.2;
     // Round to one decimal place
     return Math.round(differential * 10.0) / 10.0;
