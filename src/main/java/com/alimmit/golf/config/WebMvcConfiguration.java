dd2b67ee2deb510bc12f3b97992ab209b8f07127
tim <timothy.mila@gmail.com>
2026-03-19 21:26:03 -0500

    fix: Disable nanosecond serialization for timestamps

--- src/main/java/com/alimmit/golf/config/WebMvcConfiguration.java
+++ src/main/java/com/alimmit/golf/config/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.cfg.DateTimeFeature;
 
 @Configuration
 class WebMvcConfiguration {
@@ -11,8 +12,10 @@ class WebMvcConfiguration {
   @Bean
   JsonMapperBuilderCustomizer jackson2ObjectMapperBuilder() {
 
-    return jsonMapperBuilder ->
-        jsonMapperBuilder.changeDefaultPropertyInclusion(
-            incl -> incl.withValueInclusion(JsonInclude.Include.NON_NULL));
+    return jsonMapperBuilder -> {
+      jsonMapperBuilder.disable(DateTimeFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+      jsonMapperBuilder.changeDefaultPropertyInclusion(
+          incl -> incl.withValueInclusion(JsonInclude.Include.NON_NULL));
+    };
   }
 }
