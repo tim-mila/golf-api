1f76586295c337793f46c17d8a21c35e7753313b
tim <timothy.mila@gmail.com>
2026-03-21 09:17:42 -0500

    fix: assertion fix for instant precision differences

--- src/test/java/com/alimmit/golf/course/JpaTeeServiceImplIT.java
+++ src/test/java/com/alimmit/golf/course/JpaTeeServiceImplIT.java
@@ -75,7 +75,20 @@ class JpaTeeServiceImplIT {
         .hasFieldOrPropertyWithValue("rating", new BigDecimal("71.2"));
 
     Optional<TeeDto> read = teeService.get(created.orElseThrow().teeId());
-    assertThat(read).isPresent().contains(created.orElseThrow());
+    assertThat(read)
+        .isPresent()
+        .get()
+        .satisfies(toAssert -> {
+          final TeeDto createdDto = created.orElseThrow();
+          assertThat(toAssert.teeId()).isEqualTo(createdDto.teeId());
+          assertThat(toAssert.courseId()).isEqualTo(createdDto.courseId());
+          assertThat(toAssert.createdAt())
+              .isCloseTo(createdDto.createdAt(), within(1000, ChronoUnit.NANOS));
+          assertThat(toAssert.name()).isEqualTo(createdDto.name());
+          assertThat(toAssert.par()).isEqualTo(createdDto.par());
+          assertThat(toAssert.rating()).isEqualTo(createdDto.rating());
+          assertThat(toAssert.slope()).isEqualTo(createdDto.slope());
+        });
   }
 
   @Test
